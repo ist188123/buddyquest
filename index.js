1673e6cb46705677a8d1e9e183685d354b81e027
ist188123 <paulorosario@sapo.pt>
2018-09-05 18:16:06 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+var resposta = require('./resposta.json');
+const client = new Discord.Client();
+var jsonData = require('./raidspn.json');
+const prefix = "+";
+
+
+
+
+
+client.on("message", async (msg) => {
+   
+
+});
+
+client.login(process.env.BOT_TOKEN);
